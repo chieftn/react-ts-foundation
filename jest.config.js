c7dc6a13ccc8d85808e6fe7a54a6f86757864d2a
Rick <rkessler@microsoft.com>
2024-01-08 08:24:13 -0800

    add jest.

--- jest.config.js
+++ jest.config.js
@@ -5,14 +5,9 @@ export default {
     collectCoverageFrom: ['./src/**', '!src/index.tsx'],
     coverageReporters: ['json', 'lcov', 'text', 'clover', 'cobertura'],
     preset: 'ts-jest/presets/js-with-ts',
-    // moduleNameMapper: {
-    //     '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__ mocks __/fileMock.js',
-    //     '^@app/(.*)$': '<rootDir>/$1',
-    // },
     rootDir: 'src',
     testEnvironment: 'jest-environment-jsdom',
     transform: {
-        '^.+\\.tsx?$': 'ts-jest',
-        // process `*.tsx` files with `ts-jest`
+        '^.+\\.tsx?$': ['ts-jest', { tsconfig: `tsconfig.jest.json` }],
     },
 };
